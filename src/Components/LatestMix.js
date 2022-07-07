5a58f88bc594f64a093dc933b2a326a6b8a68c0c
Jordy Evans <jeva126@aucklanduni.ac.nz>
2022-07-07 16:59:49 +1200

    minimized - removed titles

--- src/Components/LatestMix.js
+++ src/Components/LatestMix.js
@@ -6,7 +6,7 @@ export default function LatestMix({title, genre, imageName, scLink, isDayTime}){
 
     return(
         <div >
-            <div className={isDayTime ? "Latest-title" : "Latest-title-dark"}>{title}</div>
+            {/* <div className={isDayTime ? "Latest-title" : "Latest-title-dark"}>{title}</div> */}
             {/* <div className={isDayTime ? "Latest-genre" : "Latest-genre-dark"}>{genre}</div> */}
             <a href={scLink} target="_blank" rel="noreferrer"><img className="App-latest" src={process.env.PUBLIC_URL + '/Images/' + imageName +'.jpg' } alt={title} />  </a>
         </div>
--- src/Components/MixArticle.js
+++ src/Components/MixArticle.js
@@ -7,7 +7,7 @@ export default function MixArticle({title, genre,imageName, date, scLink, isDayT
 
     return(
         <div className="MixArticle-container">
-            <div className={isDayTime ? "MixArticle-title" : "MixArticle-title-dark"}>{title}</div>
+            {/* <div className={isDayTime ? "MixArticle-title" : "MixArticle-title-dark"}>{title}</div> */}
             {/* <div className={isDayTime ? "MixArticle-genre" : "MixArticle-genre-dark"}>{genre}</div> */}
             {/* <div className="MixArticle-date">{date}</div> */}
             <a href={scLink} target="_blank" rel="noreferrer"><img className="MixArticle-photo" src={process.env.PUBLIC_URL + '/Images/' + imageName + '.jpg'} alt={title} />  </a> 
